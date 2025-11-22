4dfc40c66b1193d7f7758b0e002840ec94530187
Ibrahim Bamba <ibamba366@gmail.com>
2025-11-22 19:46:08 +0000

    Reinitialisation du projet

--- src/store/aiStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-export const useAIStore = create((set) => ({
-    triage: null,
-    recommendations: [],
-    setTriage: (t) => set({ triage: t }),
-    setRecommendations: (r) => set({ recommendations: r }),
-}));
--- src/store/paymentsStore.js
+++
@@ -1,6 +0,0 @@
-import { create } from "zustand";
-
-export const usePaymentsStore = create((set) => ({
-    payments: [],
-    setPayments: (items) => set({ payments: items }),
-}));
